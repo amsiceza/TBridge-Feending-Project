52aba3c25cd10d501b865e331214426bda9acd7d
kbastamow <bastamowkat@gmail.com>
2023-05-19 16:12:42 +0200

    Structuring + sass colors in progress

---
+++ src/components/Buttons/KatTest/KatTest.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../../sass/buttons.scss"
+import {HiDocumentCheck} from "react-icons/hi2"
+
+const KatTest = () => {
+  return (
+    <>
+    <div>KatTest</div>
+
+    <div><button type="button" className="btn-guardar-lg">    <i><HiDocumentCheck></HiDocumentCheck></i><span>button</span></button></div>
+
+    <div><button type="button" className="btn-guardar-md">guardar</button></div>
+ 
+    </>
+  )
+
+}
+
+export default KatTest
