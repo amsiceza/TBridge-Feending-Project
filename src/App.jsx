0b666207100646d01504a19d49065af2450b2cbf
[amsiceza] <[ismacerverazamora@gmail.com]>
2023-05-20 19:10:30 +0200

    Telephone screen in recinto view

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import './App.scss'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import Sidebar from './components/Sidebar/Sidebar'
 
 import KatTest from './components/Buttons/KatTest/KatTest'
-import SideBar from './components/Sidebar/Sidebar'
+import RecintoEvento from './pages/RecintoEvento/RecintoEvento'
 
 function App() {
 
   return (
-    <>
-    <BrowserRouter>
+  <div className="app-container">    
+  <BrowserRouter>
+    <Sidebar/>
     <Routes>
       <Route path="/Kat" element={<KatTest/>}/>
-      <Route path="/sideBar" element={<SideBar/>}/>
+      <Route path="/recinto" element={<RecintoEvento/>}/>
     </Routes>
     </BrowserRouter>
-    </>
+  </div>
   )
 }
 
--- src/pages/RecintoEvento/RecintoEvento.jsx
+++ src/pages/RecintoEvento/RecintoEvento.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+
+import Tlf from "../../assets/tlf.png";
+import ScreenTlf from "../../assets/screen-tlf.png";
+
+
+import "./RecintoEvento.scss";
 
 function RecintoEvento() {
   return (
-    <div>RecintoEvento</div>
-  )
+    <>
+      <div className="recinto-main-container">
+        <div className="recinto-mvl">
+          <img  className="tlf1" src={Tlf} alt="" />
+        </div>
+
+        <div className="recinto-content">
+          <h2>Recinto del evento</h2>
+          <div>
+            <div className="sendimg-container">
+              <div className="first-label-recinto">
+                <div>
+                    <p>Mapa</p>
+                    <p>
+                      Copy Carga aquí el mapa del recinto. El archivo debe tener
+                      unas dimensiones de XXpx x XXpx y estar en formato .jpg o .png
+                    </p>
+                </div>
+                <div>
+                    
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default RecintoEvento
+export default RecintoEvento;
