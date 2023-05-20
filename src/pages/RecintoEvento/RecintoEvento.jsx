089973bb3ba08439ae31dde22bb3c9d5cef617c9
[amsiceza] <[ismacerverazamora@gmail.com]>
2023-05-20 19:57:40 +0200

    Updates recinto view + udpdate buttons styles

--- src/pages/RecintoEvento/RecintoEvento.jsx
+++ src/pages/RecintoEvento/RecintoEvento.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 import Tlf from "../../assets/tlf.png";
-import ScreenTlf from "../../assets/screen-tlf.png";
+
+import { HiMagnifyingGlass, HiOutlineTrash } from "react-icons/hi2"
+
+import BtnSave from "../../components/Buttons/BtnSave"
 
 
 import "./RecintoEvento.scss";
@@ -18,18 +21,30 @@ function RecintoEvento() {
           <h2>Recinto del evento</h2>
           <div>
             <div className="sendimg-container">
+              
               <div className="first-label-recinto">
                 <div>
-                    <p>Mapa</p>
-                    <p>
-                      Copy Carga aquí el mapa del recinto. El archivo debe tener
+                    <p className="mapa">Mapa</p>
+                    <p className="mapa-text">
+                      Carga aquí el mapa del recinto. El archivo debe tener
                       unas dimensiones de XXpx x XXpx y estar en formato .jpg o .png
                     </p>
                 </div>
-                <div>
-                    
+                <div className="btn-first-lbl">
+                    <div className="icons">
+                        <HiMagnifyingGlass/>
+                        <HiOutlineTrash/>
+                    </div>
+                    <div>
+                        <BtnSave/>
+                    </div>
                 </div>
               </div>
+
+              <div className="second-label-recinto">
+                    
+              </div>
+
             </div>
           </div>
         </div>
