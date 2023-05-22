9bffa044411502de95d6100ed8c8a4b17cff18d4
Goner10 <zalito_96@hotmail.com>
2023-05-22 13:52:07 +0200

    merge fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,46 @@ import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Sidebar from "./components/Sidebar/Sidebar";
-
-import Home from './components/Buttons/Home/Home'
-import KatForm from './KatForm/KatForm'
-import KatTest from './components/Buttons/KatTest/KatTest'
-import RecintoEvento from './pages/RecintoEvento/RecintoEvento'
-import Register from './components/Register/Register'
-import Login from "./components/Login/Login";
+import Home from './components/Buttons/Home/Home';
+import KatForm from './KatForm/KatForm';
+import KatTest from './components/Buttons/KatTest/KatTest';
+import RecintoEvento from './pages/RecintoEvento/RecintoEvento';
+import Register from './components/Register/Register';
 import Header from "./components/Header/Header";
+import Login from './components/Login/Login';
 
 function App() {
+  const token = localStorage.getItem('token'); // Obtener el token del local storage
+
   return (
     <div>
       <BrowserRouter>
-      <Header/>
-      <div className="app-container">
-        
-        
-          <div className="sidebar-boss-container">
-            <Sidebar />
-          </div>
+      {!token && !window.location.pathname.includes('/register') && ( // Ternario para condicionar la renderización del Sidebar
+            <Header/>
+          )}
+        <div className="app-container">
+          {!token && !window.location.pathname.includes('/register') && ( // Ternario para condicionar la renderización del Sidebar
+            <div className="sidebar-boss-container">
+              <Sidebar />
+            </div>
+          )}
+          
           <div className="content-container">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/kat" element={<KatTest />} />
               <Route path="/katform" element={<KatForm />} />
               <Route path="/recinto" element={<RecintoEvento />} />
-              <Route path="/register" element={<Register/>} />
-              <Route path="/login" element={<Login/>} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+
             </Routes>
           </div>
-        
-      </div>
+        </div>
       </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
