a741e673a79844c8d571615db06484d4006d23e5
kbastamow <bastamowkat@gmail.com>
2023-05-23 22:17:42 +0200

    Event slice and service

---
+++ src/features/event/eventService.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080";
+
+const createEvent= async (data) => {
+  const res = await axios.post(API_URL + "/eventos/create", data);
+  console.log(res.data)
+  return res.data;
+};
+
+const eventService = {
+  createEvent,
+};
+
+export default eventService;
---
+++ src/features/event/eventSlice.js
@@ -0,0 +1,32 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import eventService from "./eventService";
+
+const initialState = {
+    event: {},
+    createMessage: ""
+}
+
+
+export const createEvent = createAsyncThunk("event/createEvent", async(data) => {
+    try {
+      return await eventService.createEvent(data);   
+    } catch (error) {
+      console.error(error);
+    }
+  })
+
+  export const eventSlice = createSlice({
+    name: "event",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+      builder
+      .addCase(createEvent.fulfilled, (state, action) => {
+        state.event = action.payload.event
+        state.createMessage = action.payload.message
+      })
+    }
+
+  })
+
+  export default eventSlice.reducer;
